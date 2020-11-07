ba60f1ba98b7b0b7ae9bdb2d2917de51ed6e47fb
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-11-07 22:09:46 +0330

    Initialize BaseRpository UnitTestCase

---
+++ tests/baserepotest.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+class BaseRepositoryTest(TestCase):
+
+    def test_get(self):
+        pass
+ 
+    def test_add(self):
+        pass
+
+    def test_remove(self):
+        pass
