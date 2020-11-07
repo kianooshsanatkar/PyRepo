cab1a3bb79d43eee556438b8a1a7f020ca2d5d77
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-11-07 22:10:45 +0330

    Implement BaseRepository Unit Test Cases

--- tests/baserepotest.py
+++ tests/baserepotest.py
@@ -1,12 +1,29 @@
 from unittest import TestCase
+from unittest.mock import Mock
+from repo.core.baserpo import BaseRepository
 
 class BaseRepositoryTest(TestCase):
 
     def test_get(self):
-        pass
+        ctx = Mock()
+        repo = BaseRepository(ctx)
+
+        with self.assertRaises(NotImplementedError):
+            repo.get('some id')
  
     def test_add(self):
-        pass
+        ctx = Mock()
+        ctx.add = Mock()
+        repo = BaseRepository(ctx)
+        obj = Mock()
+        repo.add(obj)
+        ctx.add.assert_called_once_with(obj)
 
     def test_remove(self):
-        pass
+
+        ctx = Mock()
+        ctx.remove = Mock()
+        repo = BaseRepository(ctx)
+        obj = Mock()
+        repo.remove(obj)
+        ctx.remove.assert_called_once_with(obj)
