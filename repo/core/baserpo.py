3846c339d3d7daa75f4eaf5e7d8b28528cb45f3d
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-11-07 22:08:01 +0330

    Initialize Base Repository

---
+++ repo/core/baserpo.py
@@ -0,0 +1,13 @@
+class BaseRepository:
+
+    def __init__(self, context):
+        pass
+
+    def get(self, uid): 
+        pass
+
+    def add(self, obj):
+        pass
+
+    def remove(self, obj):
+        pass
