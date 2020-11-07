b610c5bea970a4659613a102939061373fd5c597
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-11-07 22:04:40 +0330

    Initialize TestCase for UnitOfWork

---
+++ tests/uowtest.py
@@ -0,0 +1,57 @@
+from unittest import TestCase, main as unittest_main
+from unittest.mock import Mock
+from repo.core.baseuow import UnitOfWork
+from uuid import uuid4
+
+
+class UnitOfWorkTest(TestCase):
+
+    def test_enter(self):
+        ctx = Mock()
+        ctx.connect = Mock()
+
+        with UnitOfWork(ctx, {}) as uow:
+            pass
+        ctx.connect.assert_called_once()
+
+    def test_exit(self):
+        ctx = Mock()
+        ctx.close = Mock()
+
+        with UnitOfWork(ctx, {}) as uow:
+            pass
+
+        ctx.close.assert_called_once()
+
+    def test_commit(self):
+        ctx = Mock()
+        ctx.commit = Mock()
+
+        with UnitOfWork(ctx, {}) as uow:
+            uow.commit()
+        ctx.commit.assert_called_once()
+
+    def test_connect(self):
+        ctx = Mock()
+        ctx.connect = Mock()
+
+        with UnitOfWork(ctx, {}) as uow:
+            uow.close()
+            uow.connect()
+
+        self.assertEqual(2, ctx.connect.call_count)
+
+    def test_repo_call(self):
+        ctx = Mock()
+        repo = Mock(return_value=uuid4())
+        factory = {repo.__class__.__name__: repo}
+        with UnitOfWork(ctx, factory) as uow:
+            r = uow.get_repository(repo.__class__.__name__)
+            r2 = uow.get_repository(repo.__class__.__name__)
+            self.assertEqual(r, r2)
+
+        repo.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest_main()
