695765054e5a9e3d45a4984df042036f5b064cba
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-11-30 04:39:13 +0330

    refactoring: rename context rename it to a public dunder to be accessible by other classes and functions

--- repo/core/baserpo.py
+++ repo/core/baserpo.py
@@ -1,13 +1,13 @@
 class BaseRepository:
 
     def __init__(self, context):
-        self.__ctx = context
+        self.__ctx__ = context
 
     def get(self, uid): 
         raise NotImplementedError()
 
     def add(self, obj):
-        self.__ctx.add(obj)
+        self.__ctx__.add(obj)
 
     def remove(self, obj):
-        self.__ctx.remove(obj)
+        self.__ctx__.remove(obj)
