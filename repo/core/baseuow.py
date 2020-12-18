21072584ec0e7255bbdae184d2eb78c41801b7fe
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-12-18 22:27:07 +0330

    Refactoring + add function: add add_repository() + change Context name and make it public inner variable instead of private variable

--- repo/core/baseuow.py
+++ repo/core/baseuow.py
@@ -1,6 +1,6 @@
 class UnitOfWork:
 
-    __repo = {}
+    __repo__ = {}
 
     @staticmethod
     def add_repositories(repositories: dict):
@@ -8,13 +8,28 @@ class UnitOfWork:
 
         Args:
 
-            repose (dict): repositories as dictionary. e.g. {'repository_class_name': RepositoryClass, ...}
+            :param repositories: repositories as dictionary. e.g. {'repository_class_name': RepositoryClass, ...}
         """
-        UnitOfWork.__repo.update(repositories)
+        if type(repositories) is not dict:
+            raise ValueError('repositories must be a dictionary')
+        UnitOfWork.__repo__.update(repositories)
+
+    @staticmethod
+    def add_repository(repository_name: str, repository: type):
+        """Add One repository to dictionary
+        
+        Args:
+
+            repository_name (str): the name of repository as a key dictionary, for repository with the same name use module name separated with dot (.)
+
+            repository (type): repository class that needed to implement, for complex object please use factory or partial approach
+            
+        """
+        UnitOfWork.__repo__[repository_name] = repository
 
     def __init__(self, _context, auto_commit=False):
         super().__init__()
-        self.__ctx = _context
+        self.__ctx__ = _context
         self.pool = {}
 
     def get_repository(self, repo: str):
@@ -35,21 +50,21 @@ class UnitOfWork:
         """
         r = self.pool.get(repo)
         if not r:
-            _ = self.__repo.get(repo)
-            r = _(self.__ctx)
+            _ = self.__repo__.get(repo)
+            r = _(self.__ctx__)
             self.pool[repo] = r
         if not r:
             raise KeyError("Repository does not exist!")
         return r
 
     def commit(self):
-        self.__ctx.commit()
+        self.__ctx__.commit()
 
     def close(self):
-        self.__ctx.close()
+        self.__ctx__.close()
 
     def connect(self):
-        self.__ctx.connect()
+        self.__ctx__.connect()
 
     def __enter__(self, *args):
         self.connect()
@@ -59,4 +74,4 @@ class UnitOfWork:
         self.close()
 
     def __repr__(self):
-        return self.__class__.__name__ + "(" + str(self.__ctx) + ")"
+        return self.__class__.__name__ + "(" + str(self.__ctx__) + ")"
